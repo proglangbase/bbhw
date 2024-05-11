f383f9fd016336b9c3f2ba869c4c3e9a6dc55adc
Christopher Augustus <repos@c4augustus.com>
2024-05-11 15:23:04 +0100

    create first Chibi Scheme implementation

---
+++ code/scheme/chibi/bbhw.scm
@@ -0,0 +1,29 @@
+;;;
+;;; Copyright © 2024 Christopher Augustus
+;;;
+;;; This Source Code Form is subject to the terms of the Mozilla Public
+;;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;;
+(import (scheme small) ;;; required for command-line, define, display
+        (srfi 18))     ;;; required for thread-sleep!
+
+(define count #f)
+(define input (cadr (append (command-line) (list ""))))
+(do () (count)
+    (if (string=? input "")
+        (begin (display "countdown: ")
+               (set! input (read-line))))
+    (set! count (string->number input))
+    (if (or (not (integer? count)) (< count 0))
+        (begin (display (string-append "Invalid countdown " input ", try again...\n"))
+               (set! count #f)
+               (set! input "")))
+)
+(display "World, Hello...")
+(flush-output-port)
+(do ((i count (- i 1))) ((zero? i))
+    (begin (display (string-append (number->string i) "...")))
+           (flush-output-port)
+           (thread-sleep! 1))
+(display "Bye Bye.\n")
