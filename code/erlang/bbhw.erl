46c612a4298bb6bf00b27aaead0cdfc5d2e5f916
Steve Thames <sthames42@gmail.com>
2024-07-18 15:59:03 -0700

    Modified bbhw.erl to be less segmented.

--- code/erlang/bbhw.erl
+++ code/erlang/bbhw.erl
@@ -1,68 +1,43 @@
 -module(bbhw).
 -export([main/0, main/1]).
 
-main()    -> main([]).             % `erl -noshell -s bbhw main [countdown] -s init stop`
-main([])  -> run();                % `escript bbhw.erl [countdown]`
-                                   % when countdown is not supplied.
+isBadArg(S) -> io:format("Invalid countdown ~p, try again...\n", [S]).
+isBadNum(N) -> isBadArg(lists:flatten(io_lib:format("~p", [N]))).
 
-main(Arg) when is_integer(Arg) ->  % `bbhw:main(<nnn>)` from shell.
-  case isItGood(Arg) of
-    true  -> run(Arg);
-    false -> isBadArg(Arg)
-  end;
-  
-main(Arg) ->
-  Is_string = io_lib:printable_list(Arg),
-  Is_list   = is_list(Arg),    
-  Is_atom   = is_atom(Arg),        % `erl -noshell` args come in as atoms.
-  
-  if 
-    Is_string -> 
-      case getCountdown(Arg) of
-        error -> main();
-        C     -> run(C)
-      end;
-      
-    Is_list -> main(hd(Arg));      % true for `Is_string`.
-    Is_atom -> main(atom_to_list(Arg));
-    true    -> isBadArg(Arg)
-  end.
-  
-getCountdown(S) ->
-  case string:to_integer(S) of
-    {I, []} -> case isItGood(I) of 
-      true -> I; 
-      _    -> isNotGood(I) 
-    end;
-    _ -> isNotGood(S)
-  end.
-   
-isBadArg(Arg) ->
-  isNotGood(Arg),
-  run().
-
-isItGood(I) -> 
-  I >= 0.
-
-isNotGood(S) -> 
-  io:format("Invalid countdown ~p, try again...\n", [S]), 
-  error.
-    
-readInput() ->
+main() ->
   case io:get_line("countdown: ") of
-    eof        -> readInput();
-    {error, _} -> readInput();
-    "\n"       -> readInput();
-    Line       -> case getCountdown(string:strip(Line, right, $\n)) of
-      error -> readInput();
-      I     -> I
-    end
+    eof        -> main();
+    {error, _} -> main();
+    "\n"       -> main();
+    Line       -> main(string:strip(Line, right, $\n))
   end.
 
-run()  -> run(readInput()).    
-run(C) ->
+main(A) when is_atom(A)  -> main(atom_to_list(A));
+main(F) when is_float(F) -> isBadNum(F), main();
+
+main(L) when is_list(L) ->
+  case io_lib:printable_list(L) of
+    true  -> 
+      case string:len(L) > 0 of
+        true  -> 
+          case string:to_integer(L) of
+            {I, []} -> main(I);
+            _       -> isBadArg(L), main()
+          end;
+        false -> main()
+      end;
+    false -> main(hd(L))
+  end;
+
+main(I) when is_integer(I) ->  
+  case I >= 0 of
+    true  -> run(I);
+    false -> isBadNum(I), main()
+  end.
+
+run(Count) ->
   io:format("World, Hello..."),
-  rundown(C),
+  rundown(Count),
   io:format("Bye Bye.\n").
   
 rundown(0) -> ok;
