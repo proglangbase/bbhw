da8777986aaa3ae934b8bd1124b99ec6343ae22d
Sami Rosendahl <sami.rosendahl@gmail.com>
2024-07-18 22:44:24 +0300

    Guile R7RS BBHW immutable do version

--- code/scheme/guile-r7rs/bbhw.scm
+++ code/scheme/guile-r7rs/bbhw.scm
@@ -3,29 +3,23 @@
         (scheme read)
         (srfi 18))
 
-(define (valid-count? count)
-  (and (integer? count) (>= count 0)))
-
-(define (prompt-count)
-  (write-string "countdown: ")
-  (let* ((input (read-line))
-         (count (string->number input)))
-    (if (valid-count? count)
-        count
-        (begin (write-string "Invalid countdown ")
-               (write-string input)
-               (write-string ", try again...\n")
-               (prompt-count)))))
-
 (define count
-  (let ((from-cmdline (string->number (cadr (append (command-line) (list ""))))))
-    (if (valid-count? from-cmdline)
-        from-cmdline
-        (prompt-count))))
+  (do ((input (cadr (append (command-line) (list "")))
+              (begin
+                (write-string "countdown: ")
+                (read-line))))
+      ((let ((count (string->number input)))
+         (and (integer? count) (>= count 0)))
+       (string->number input))
+    (unless (string=? input "")
+      (write-string "Invalid countdown ")
+      (write-string input)
+      (write-string ", try again...")
+      (newline))))
 
-(display "World, Hello...") (flush-output-port)
+(write-string "World, Hello...") (flush-output-port)
 (do ((i count (- i 1)))
     ((zero? i))
   (display i) (write-string "...") (flush-output-port)
   (thread-sleep! (seconds->time (+ (time->seconds (current-time)) 1))))
-(display "Bye Bye.\n")
+(write-string "Bye Bye.") (newline)
