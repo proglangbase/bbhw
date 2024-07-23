decaf6c9f1baaceb5ea6e45f1e13876d73067354
Steve Thames <sthames42@gmail.com>
2024-07-23 16:22:35 -0700

    Completed refactoring of Erlang and Elixir versions.

--- code/erlang/bbhw.erl
+++ code/erlang/bbhw.erl
@@ -1,43 +1,40 @@
 -module(bbhw).
 -export([main/0, main/1]).
 
-isBadArg(S) -> io:format("Invalid countdown ~p, try again...\n", [S]), main().
-isBadNum(N) -> isBadArg(lists:flatten(io_lib:format("~p", [N]))).
+badArg(S) -> io:format("Invalid countdown ~p, try again...\n", [S]), main().
+badNum(N) -> badArg(lists:flatten(io_lib:format("~p", [N]))).
 
 main() ->
   case io:get_line("countdown: ") of
     eof        -> main();
     {error, _} -> main();
-    "\n"       -> main();
     Line       -> main(string:chomp(Line))
   end.
 
 main([]) -> main();
 
-main(A) when is_atom(A)            -> main(atom_to_list(A));
-main(F) when is_float(F)           -> isBadNum(F);
-main(I) when is_integer(I), I <  0 -> isBadNum(I);
-main(I) when is_integer(I), I >= 0 -> run(I);
+main(F) when is_float(F)          -> badNum(F);
+main(I) when is_integer(I), I < 0 -> badNum(I);
+main(A) when is_atom(A)           -> main(atom_to_list(A));
 
 main(L) when is_list(L) ->
   case io_lib:printable_list(L) of
     true ->
       case string:to_integer(L) of
         {I, []} -> main(I);
-        _       -> isBadArg(L)
+        _       -> badArg(L)
       end;
     false -> main(lists:concat(lists:join(" ", L)))
   end;
 
-main(Unk) -> isBadArg(Unk).
-
-run(Count) ->
+main(Count) when is_integer(Count), Count >= 0 -> 
+  Loop = fun Loop(0) -> ok; Loop(I) ->
+    io:format("~B...", [I]),
+    timer:sleep(1000), 
+    Loop(I-1)
+  end,
   io:format("World, Hello..."),
-  rundown(Count),
-  io:format("Bye Bye.\n").
-  
-rundown(0) -> ok;
-rundown(C) ->
-  io:format("~B...", [C]),
-  timer:sleep(1000), 
-  rundown(C-1).
+  Loop(Count),
+  io:format("Bye Bye.\n");
+
+main(Unk) -> badArg(Unk).
