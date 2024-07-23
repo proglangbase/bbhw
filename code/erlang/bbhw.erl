fe599670a19d91b9b955197aceaa293cbcae97fb
Steve Thames <sthames42@gmail.com>
2024-07-22 17:39:07 -0700

    Integrated c4 Erlang suggestions to Erlang and Elixir.

--- code/erlang/bbhw.erl
+++ code/erlang/bbhw.erl
@@ -1,7 +1,7 @@
 -module(bbhw).
 -export([main/0, main/1]).
 
-isBadArg(S) -> io:format("Invalid countdown ~p, try again...\n", [S]).
+isBadArg(S) -> io:format("Invalid countdown ~p, try again...\n", [S]), main().
 isBadNum(N) -> isBadArg(lists:flatten(io_lib:format("~p", [N]))).
 
 main() ->
@@ -9,31 +9,27 @@ main() ->
     eof        -> main();
     {error, _} -> main();
     "\n"       -> main();
-    Line       -> main(string:strip(Line, right, $\n))
+    Line       -> main(string:chomp(Line))
   end.
 
-main(A) when is_atom(A)  -> main(atom_to_list(A));
-main(F) when is_float(F) -> isBadNum(F), main();
+main([]) -> main();
+
+main(A) when is_atom(A)            -> main(atom_to_list(A));
+main(F) when is_float(F)           -> isBadNum(F);
+main(I) when is_integer(I), I <  0 -> isBadNum(I);
+main(I) when is_integer(I), I >= 0 -> run(I);
 
 main(L) when is_list(L) ->
   case io_lib:printable_list(L) of
-    true  -> 
-      case string:len(L) > 0 of
-        true  -> 
-          case string:to_integer(L) of
-            {I, []} -> main(I);
-            _       -> isBadArg(L), main()
-          end;
-        false -> main()
+    true ->
+      case string:to_integer(L) of
+        {I, []} -> main(I);
+        _       -> isBadArg(L)
       end;
-    false -> main(hd(L))
+    false -> main(lists:concat(lists:join(" ", L)))
   end;
 
-main(I) when is_integer(I) ->  
-  case I >= 0 of
-    true  -> run(I);
-    false -> isBadNum(I), main()
-  end.
+main(Unk) -> isBadArg(Unk).
 
 run(Count) ->
   io:format("World, Hello..."),
