dd3e7dfc2bc2ecf17d79d98399bf53c7df2c1d2c
Steve Thames <sthames42@gmail.com>
2024-07-23 16:22:35 -0700

    Completed refactoring of Erlang and Elixir versions.

--- code/elixir/bbhw.ex
+++ code/elixir/bbhw.ex
@@ -1,6 +1,6 @@
 defmodule BBHW do
 
-  defp isBadArg(v), do: (IO.puts("Invalid countdown \"#{v}\", try again..."); main())
+  defp badArg(v), do: (IO.puts("Invalid countdown \"#{v}\", try again..."); main())
   
   def main() do
     case IO.gets("countdown: ") |> String.trim() do
@@ -11,32 +11,29 @@ defmodule BBHW do
 
   def main(""), do: main()
 
-  def main(a) when is_atom(a),               do: isBadArg(a)
-  def main(f) when is_float(f),              do: isBadArg(f)
-  def main(l) when is_list(l),               do: main(Enum.join(l, " "))
-  def main(i) when is_integer(i) and i <  0, do: isBadArg(i)
-  def main(i) when is_integer(i) and i >= 0, do: run(i)
+  def main(a) when is_atom(a),              do: badArg(a)
+  def main(f) when is_float(f),             do: badArg(f)
+  def main(i) when is_integer(i) and i < 0, do: badArg(i)
+  def main(l) when is_list(l),              do: main(Enum.join(l, " "))
 
   def main(s) when is_binary(s) do
     case Integer.parse(s) do
       {i, ""} -> main(i);
-      _       -> isBadArg(s)
+      _       -> badArg(s)
     end
   end
 
-  def main(Unk), do: isBadArg(Unk)
-  
-  defp run(count) do
+  def main(count) when is_integer(count) and count >= 0 do
+    loop = fn (0, _) -> :ok; (i, f) ->
+      IO.write("#{i}...")
+      Process.sleep(1000) 
+      f.(i-1, f)
+    end
     IO.write("World, Hello...")
-    rundown(count)
+    loop.(count, loop)
     IO.puts("Bye Bye.")
   end
-    
-  defp rundown(0), do: :ok
-  defp rundown(count) do
-    IO.write("#{count}...")
-    Process.sleep(1000) 
-    rundown(count-1)
-  end
+
+  def main(Unk), do: badArg(Unk)
 end
 
