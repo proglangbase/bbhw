77b17e98c56b07adb9fe0171eafec99ce7ec26fe
Christopher Augustus <repos@c4augustus.com>
2024-08-24 23:09:09 +0100

    refactor gambit scheme a bit

--- code/scheme/gambit/bbhw-sub.scm
+++ code/scheme/gambit/bbhw-sub.scm
@@ -8,21 +8,22 @@
 ;;;
 (import (srfi 48)) ; required for format
 
-(define (cmd-line-arg)        (cadr (append (command-line) (list ""))))
-(define (ask-count)           (display "countdown: ") (read-line))
-(define (valid-count n)       (and (integer? n) (>= n 0)))
-(define (say-invalid s)       (display (format "Invalid countdown ~s, try again...\n" s)))
-(define (count-or-ask-rec s)  (let ((n (string->number s)))
-                                   (if (valid-count n) n
-                                       (begin (say-invalid s)
-                                              (count-or-ask-rec (ask-count))))))
-(define (count-or-ask s)      (count-or-ask-rec (if (string=? s "") (ask-count) s)))
-(define (get-count)           (count-or-ask (cmd-line-arg)))
-(define (say-hello)           (display "World, Hello...")  (flush-output-port))
-(define (say-second s)        (display (format "~a..." s)) (flush-output-port))
-(define (say-second-sleep s)  (say-second s) (thread-sleep! 1))
-(define (count-down n)        (do ((i n (- i 1))) ((zero? i)) (say-second-sleep i)))
-(define (say-bye-bye)         (display "Bye Bye.\n"))
-(define (after x f g)         (f) (g x))
+(define (cmd-line-arg)    (cadr (append (command-line) (list ""))))
+(define (ask-count)       (display "countdown: ") (read-line))
+(define (valid-count n)   (and (integer? n) (>= n 0)))
+(define (say-invalid s)   (display (format "Invalid countdown ~s, try again...\n" s)))
+(define (count-or-ask s)  (let ((n (string->number s)))
+                               (if (valid-count n) n
+                                   (begin (say-invalid s)
+                                          (count-or-ask (ask-count))))))
+(define (ask-if-empty s)  (if (string=? s "") (ask-count) s))
+(define (get-count)       (count-or-ask (ask-if-empty (cmd-line-arg))))
+(define (say-hello)       (display "World, Hello...")  (flush-output-port))
+(define (say-sec s)       (display (format "~a..." s)) (flush-output-port))
+(define (say-sec-sleep s) (say-sec s) (thread-sleep! 1))
+(define (count-down n)    (do ((i n (- i 1))) ((zero? i)) (say-second-sleep i)))
+(define (say-bye-bye)     (display "Bye Bye.\n"))
+(define (after x f g)     (f) (g x))
+  ;;; ^ similar to dyadic after in BQN but f and g ignore w
 
 (after (get-count) say-hello count-down) (say-bye-bye)
